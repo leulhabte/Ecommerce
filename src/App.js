144249e127f4c2e225da87a8a07750da40764152
jhon <ethiofanjo@gmail.com>
2021-06-12 14:26:58 -0400

    giving a classname
    giving a classname for each components
    in order to avoid code overlapping and to go with the exact sequence.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import React from 'react'
+import {Typography, Button, Card, CardActions, CssBaseline, Toolbar, Container, Grid, CardContent, CardMedia, AppBar,} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) =>({
+
+
+
+}));
+export default useStyles;
+
 
 const App = () => {
     return (
         <div>
-            home
-        </div>
-    )
-}
+          <>
+          <header>
+                <nav> </nav>
 
-export default App
+                
+          </header>
+          <main>
+              <div className="bestseller">
+                <Container>
+
+                </Container>
+              </div>
+              <div className="face">
+
+              </div>
+              <div classname="lips">
+
+              </div>
+              <div className="eyes">
+
+              </div>
+              <div classname="skins">
+
+              </div>
+              <div classname="burshes">
+
+              </div>
+          </main>
+          <footer>
+
+          </footer>
+          </>
+        </div>
+    );
+};
+
+export default App;
 
